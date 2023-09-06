8c6aa96a26f0e9a836266640b770d9f60a02ff90
那个念想 <595875750@qq.com>
2023-09-06 09:43:40 +0800

    feat: 优化@别名的引用

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 // 如果编辑器提示 path 模块找不到，则可以安装一下 @types/node -> npm i @types/node -D
-import { resolve } from 'path';
+import { resolve } from 'node:path';
 import VueSetupExtend from 'vite-plugin-vue-setup-extend'; //方便定义组件的name命名
 
 // https://vitejs.dev/config/
